210a78d75648121f4b984371f9fb204ca899905a
safadtm <safadtm2004@gmail.com>
2022-09-07 10:23:03 +0530

    All Notes UI done

---
+++ lib/view/screen_all_notes.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ScreenAllNotes extends StatelessWidget {
+  const ScreenAllNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('All Notes'),
+      ),
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          children: List.generate(
+              10,
+              (index) => NoteItem(
+                    id: index.toString(),
+                    title: 'lores Ipsum Title $index',
+                    content:
+                        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia',
+                  )),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+                builder: (ctx) => ScreenAddNote(type: ActionType.addNote)),
+          );
+        },
+        label: const Text('New'),
+        icon: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String content;
+
+  const NoteItem(
+      {super.key,
+      required this.id,
+      required this.title,
+      required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => ScreenAddNote(
+              type: ActionType.editNote,
+              id: id,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey),
+        ),
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    title,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
+            Text(
+              content,
+              maxLines: 5,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(color: Colors.black, fontSize: 14),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
